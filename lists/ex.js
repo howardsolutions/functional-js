bd4d0a07f6d3fe79948abb45c3c7b5d58bfca922
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-02 12:17:52 -0700

    list: solution 1-4

--- lists/ex.js
+++ lists/ex.js
@@ -1,3 +1,38 @@
-"use strict";
+'use strict';
 
 // Put your code here! :)
+function six() {
+  return 6;
+}
+
+function nine() {
+  return 9;
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+// add(six(), nine());
+
+// 3)
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+// 4)
+function constant(x) {
+  return function f() {
+    return x;
+  };
+}
+
+const ten = constant(10);
+// ten() => return 10
+
+// 5) 
+function addn(fns) {
+    
+}
+
+// addn(constant(3), constant(7),...)
